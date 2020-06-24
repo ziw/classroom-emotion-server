ca71eee681c307bec52bc6ffa66e8c80c9af4ea3
Zi Wang <wangzi0112@gmail.com>
2020-06-24 14:57:19 -0700

    add training model from face-api.js to static assets

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'models'))
   await app.listen(8080);
 }
 bootstrap();
